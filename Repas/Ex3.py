061e9ac7f508b62c3cfd88e029c0a6830a9aa508
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-01-24 18:52:59 +0100

    Ex3 fet

---
+++ Repas/Ex3.py
@@ -0,0 +1,7 @@
+print("Digues dos valors i et retornare el mes gran")
+valor1 = float(input("Digues el primer valor  "))
+valor2 = float(input("Digues el segon valor  "))
+if(valor1>=valor2):
+    print(str(valor1))
+else:
+    print(str(valor2))
