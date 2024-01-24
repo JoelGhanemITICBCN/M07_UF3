1582f11ea333936120d931698b1a0f9f12e74d7c
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-01-24 18:56:12 +0100

    Ex 4 fet

---
+++ Repas/Ex4.py
@@ -0,0 +1,5 @@
+print("Digues 2 valors i els sumare per tu ")
+valor1 = float(input("Digues el primer valor  "))
+valor2 = float(input("Digues el segon valor  "))
+valorTotal = valor1 + valor2
+print(str(valorTotal))
