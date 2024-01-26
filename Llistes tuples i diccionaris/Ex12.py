b5150570285837500d4f5ba25c2e73b1f8a30b29
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-01-26 17:26:03 +0100

    A casa

---
+++ Llistes tuples i diccionaris/Ex12.py
@@ -0,0 +1,4 @@
+tupla = tuple("Gener","Febrer","Març","Abril","Maig","Juny","Juliol","Agost","Setembre","Octubre","Novembre","Decembre")
+numUsuari = int(input("Digues un número entre l'1 i el 12       "))
+if(numUsuari == 0):
+    
