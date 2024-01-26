180e2c0ab8513afd8bd09fd5c858efa8e538ac1d
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-01-26 16:02:57 +0100

    Ex9 fet

---
+++ Repas/Ex9.py
@@ -0,0 +1,5 @@
+paraula1 = input("Digues una paraula     ")
+paraula2 = input("Digues una més     ")
+paraula1Nova = paraula2[:2] + paraula1[2:]
+paraula2Nova = paraula1[:2] + paraula2[2:]
+print("La primera paraula canviada: " + paraula1Nova + " La segona paraula canviada: " + paraula2Nova)
