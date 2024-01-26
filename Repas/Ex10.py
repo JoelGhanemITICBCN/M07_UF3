b5ff4dd73df52420fcd217b1a4c676f9d484bf86
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-01-26 17:06:50 +0100

    Ex 10 bonito

--- Repas/Ex10.py
+++ Repas/Ex10.py
@@ -1,11 +1,23 @@
 import random
 num = random.randint(1,100)
+numMesGran = 0
 ##print(num)
 print("Has d'endevinar un número entre l'1 i el 100")
 numJugador = int(input("Digues un número   "))
+numMesPetit = numJugador
 while (num != numJugador):
     if (num > numJugador):
-        numJugador = int(input("Digues un més gran   "))
+        if(numMesGran <= numJugador):
+            numMesGran = numJugador
+        numJugador = int(input("Digues un del " + str(numMesGran) + " al 100   "))
     else:
-        numJugador = int(input("Digues un més baix   "))
+        """
+        print("Num mes petit")
+        print(numMesPetit)
+        print("numJugador")
+        print(numJugador)
+        """
+        if(numMesPetit >= numJugador):
+            numMesPetit = numJugador
+        numJugador = int(input("Digues un del " + str(numMesPetit) + " al 0   "))
 print("Correcte! el numero era " + str(num))
