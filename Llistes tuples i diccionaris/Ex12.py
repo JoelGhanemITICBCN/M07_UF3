8d123a9c6d7fc63ca09a89d487d75518578a2ead
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-01-31 16:59:04 +0100

    Ex12 Fet

--- Llistes tuples i diccionaris/Ex12.py
+++ Llistes tuples i diccionaris/Ex12.py
@@ -1,4 +1,6 @@
-tupla = tuple("Gener","Febrer","Març","Abril","Maig","Juny","Juliol","Agost","Setembre","Octubre","Novembre","Decembre")
+tupla = tuple(["Gener","Febrer","Març","Abril","Maig","Juny","Juliol","Agost","Setembre","Octubre","Novembre","Decembre"])
 numUsuari = int(input("Digues un número entre l'1 i el 12       "))
-if(numUsuari == 0):
-    
+while(numUsuari != 0)    :
+    print(tupla[numUsuari-1])
+    numUsuari = int(input("Digues un altre número entre l'1 i el 12       "))
+print ("adeu")
