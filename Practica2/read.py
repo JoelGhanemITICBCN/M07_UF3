a00cdfbacead614b2a385003afc2ecff2a61893e
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-02-11 14:14:40 +0100

    Select funcional

--- Practica2/read.py
+++ Practica2/read.py
@@ -1,14 +1,19 @@
 #Read de la base de datos
+#Comprobado por separado que funciona
 import connection
 
 def select():
+    #query
         select = '''
         select * from pokemon
         '''
-        connection.execute(select)
-        resultados = connection.fetchall()
+        #Ejecucion y aplicacion de la query
+        connection.conection.execute(select)
+        resultados = connection.conection.fetchall()
+        #Si la lista no esta vacia, muestra todos los datos
         if len(resultados) > 0:
             for resultado in resultados:
                 print(resultado)
-            else: 
-                print("Esta vacio")
+        #Si esta vacia dice que esta vacia
+        else: 
+            print("La tabla esta vacia")
