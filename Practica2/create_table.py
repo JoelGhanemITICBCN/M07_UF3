1fb5473d0978206006e2805f92f276b02d0d9d30
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-02-11 14:13:52 +0100

    Creacion de la tabla funcional

--- Practica2/create_table.py
+++ Practica2/create_table.py
@@ -1,15 +1,20 @@
 #Creacion de la tabla
+#Comprobado que funciona por separado
 import connection
 def createTable():
+        #query
+        dropTable = '''DROP TABLE IF EXISTS pokemon'''
         createTable = '''
                         CREATE TABLE if not exists pokemon(
                         id SERIAL PRIMARY KEY,
                         name VARCHAR(255) NOT NULL,
                         type VARCHAR(255) NOT NULL,
-                        weight varchar(255) NOT NULL,
+                        weight BIGINT NOT NULL,
                         lvl VARCHAR(255) NOT NULL,
                         height VARCHAR(255) NOT NULL
         )'''
-        connection.execute(createTable)
-        connection.commit()
+        #Ejecucion de la query con el commit para que se aplique
+        connection.conection.execute(dropTable)
+        connection.conection.execute(createTable)
+        connection.conn.commit()
         print("Tabla creada bien: " + createTable)
