d8fae9b205114aa858380eb3c2374d987ab49a1e
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2024-02-11 12:33:33 +0100

    Update millorat

--- Practica2/update.py
+++ Practica2/update.py
@@ -1,1 +1,15 @@
 #Update de la base de datos
+import connection
+
+def update():
+    update = '''
+        update pokemon set 
+        name = 'Squirtle',
+        type = 'Agua',
+        weight = '80 kg',
+        height = '1,1 m',
+        where name = 'pikachu';
+'''
+    connection.execute(update)
+    connection.commit()
+    print(f"Se ha actualizado tu pokemon")
