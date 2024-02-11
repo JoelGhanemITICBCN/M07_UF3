e0b57f3f016810734fc85812c0b26d2ed5b82de0
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-02-11 14:14:06 +0100

    Insert funcional

--- Practica2/create.py
+++ Practica2/create.py
@@ -1,13 +1,14 @@
 #insert de sql
+#Comprobado que funciona por separado
 import connection
-from connection import conecta
 def create():
-    conn = connection.conecta()
-    cursor = conn.cursor()
+    #query
     insert = ''' 
         insert into pokemon (name, type,weight,lvl,height) 
-        values('pikachu','electric',12 kg,lvl 100,1 m)
+        values('pikachu','electric',12,'lvl 100','1 m')
                  '''
-    cursor.execute(insert)
-    conn.commit()
+    #Ejecucion y aplicacionn de dicha query
+    connection.conection.execute(insert)
+    connection.conn.commit()
+    #Mensaje de todo okei
     print("Insert bien: " + insert)
