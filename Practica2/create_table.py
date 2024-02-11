689fb923277c0c91f5d639eac90bdce6be291c79
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2024-02-11 12:32:13 +0100

    Creacuio de taula millorada

--- Practica2/create_table.py
+++ Practica2/create_table.py
@@ -1,22 +1,15 @@
 #Creacion de la tabla
 import connection
 def createTable():
-    try:
-        conn = connection.conecta()
-        cursor = conn.cursor()
-        createTable = '''CREATE TABLE if not exists pokemon(
+        createTable = '''
+                        CREATE TABLE if not exists pokemon(
                         id SERIAL PRIMARY KEY,
                         name VARCHAR(255) NOT NULL,
                         type VARCHAR(255) NOT NULL,
-                        weight BIGINT NOT NULL,
+                        weight varchar(255) NOT NULL,
                         lvl VARCHAR(255) NOT NULL,
                         height VARCHAR(255) NOT NULL
         )'''
-        cursor.execute(createTable)
-        conn.commit()
+        connection.execute(createTable)
+        connection.commit()
         print("Tabla creada bien: " + createTable)
-    except Exception as e:
-        print("Error al crear tabla: ",e) 
-    finally:
-        cursor.close()
-        conn.close()
