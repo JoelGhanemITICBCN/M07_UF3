f52206a64e92babf24ea6035b3a057b3812c1f58
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-02-11 14:14:27 +0100

    Delete funcional

--- Practica2/delete.py
+++ Practica2/delete.py
@@ -1,8 +1,12 @@
 #query de borrado de un campo
+#Comprovado por separado que funciona
 import connection
 def delete():
+    #query 
     borra = '''
         delete from pokemon'''
-    connection.execute(borra)
-    connection.commit()
+    #Ejecucion y aplicacion de dicha query
+    connection.conection.execute(borra)
+    connection.conn.commit()
+    #Mensaje de confirmacion
     print("Borrados los elementos de la tabla")
